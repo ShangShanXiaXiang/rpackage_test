f4461a7fb8c7f30010ee76e65613a6747b683a7b
ShangShanXiaXiang <crimsonpeter317@gmail.com>
2023-11-01 21:10:21 -0400

    changing Describe Data but unable to generate documents.

--- R/DescribeData.R
+++ R/DescribeData.R
@@ -1,3 +1,11 @@
-library(c(psych,mtcars))
+library(psych)
 
 describe(mtcars)
+
+square_compute = function(x){
+  return(x^2)
+}
+
+for (i in 1:length(names(mtcars))) {
+  print(sum(square_compute(mtcars[,i])))
+}
