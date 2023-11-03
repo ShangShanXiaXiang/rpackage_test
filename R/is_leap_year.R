dbeec5c2a2c7d97212599603d5ef02ef488b5951
ShangShanXiaXiang <crimsonpeter317@gmail.com>
2023-11-03 00:13:16 -0400

    adding roxygen2 parameters

--- R/is_leap_year.R
+++ R/is_leap_year.R
@@ -1,5 +1,8 @@
-
-
+#' This is a function to check for leap years
+#'
+#' @param year The year to check
+#' @return TRUE if it's a leap year, FALSE otherwise
+#' @export
 is_leap_year <- function(year) {
   # Decide a given year is a leap year or not
   if (year %% 4 != 0) {
